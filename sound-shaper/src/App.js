0054ae79108e5d28850e5e16f29095c8c6f65a8d
femtoF121 <yukhym.kashchenko@nure.ua>
2023-12-08 23:53:29 +0200

    added loader

--- sound-shaper/src/App.js
+++ sound-shaper/src/App.js
@@ -1,22 +1,104 @@
-import React, {memo, startTransition, useEffect, useState, useTransition} from "react";
-import {Header} from "./modules/Header";
-import {Main} from "./modules/Main";
-import {Footer} from "./modules/Footer";
+import React, { memo, startTransition, useEffect, useState } from "react";
+import { Header } from "./modules/Header";
+import { Main } from "./modules/Main";
+import { Footer } from "./modules/Footer";
+import { motion } from "framer-motion";
+
+const loadingContainer = {
+	width: "8rem",
+	height: "8rem",
+	display: "flex",
+	justifyContent: "space-around",
+};
+const loadingCircle = {
+	display: "block",
+	width: "2rem",
+	height: "2rem",
+	borderRadius: "100%",
+};
+const firstCircle = {
+	backgroundColor: "#DA4FC4",
+};
+const secondCircle = {
+	backgroundColor: "#A72693",
+};
+const thirdCircle = {
+	backgroundColor: "#7B146B",
+};
+
+const loadingContainerVariants = {
+	start: {
+		transition: {
+			staggerChildren: 0.2,
+		},
+	},
+	end: {
+		transition: {
+			staggerChildren: 0.2,
+		},
+	},
+};
+
+const loadingCircleVariants = {
+	start: {
+		y: "0%",
+	},
+	end: {
+		y: "60%",
+	},
+};
+const loadingCircleTransition = {
+	duration: 0.6,
+	repeat: Infinity,
+	repeatType: "reverse",
+	ease: "easeInOut",
+};
+
+const Loader = () => {
+	return (
+		<div>
+			<div className="fixed  w-full min-h-screen z-50 bg-black opacity-30" />
+			<div className="flex fixed w-full justify-center items-center h-screen">
+				<motion.div
+					style={loadingContainer}
+					variants={loadingContainerVariants}
+					initial="start"
+					animate="end"
+				>
+					<motion.span
+						style={{ ...loadingCircle, ...firstCircle }}
+						variants={loadingCircleVariants}
+						transition={loadingCircleTransition}
+					></motion.span>
+					<motion.span
+						style={{ ...loadingCircle, ...secondCircle }}
+						variants={loadingCircleVariants}
+						transition={loadingCircleTransition}
+					></motion.span>
+					<motion.span
+						style={{ ...loadingCircle, ...thirdCircle }}
+						variants={loadingCircleVariants}
+						transition={loadingCircleTransition}
+					></motion.span>
+				</motion.div>
+			</div>
+		</div>
+	);
+};
 
 export const App = memo(() => {
-    const [loading, setLoading] = useState(true)
-    useEffect(() => startTransition(() => setLoading(false)), [])
-    return (
-        <div className="flex flex-col w-full h-[100vh] px-16 py-8 bg-myBlue-200 text-myYellow">
-            {
-                loading
-                    ? <div>Loading</div>
-                    : <>
-                        <Header/>
-                        <Main/>
-                        <Footer/>
-                    </>
-            }
-        </div>
-    );
+	const [loading, setLoading] = useState(true);
+	const styles = "flex flex-col w-full h-[100vh] bg-myBlue-200 text-myYellow";
+	useEffect(() => startTransition(() => setLoading(false)), []);
+	return loading ? (
+		<div className={styles}>
+			<Loader />
+		</div>
+	) : (
+		<div className={styles + " px-16 py-8"}>
+			<Header />
+			<Main />
+			<Footer />
+		</div>
+	);
 });
